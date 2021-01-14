bd58143c64c82c7eb7ca61db67a469cb865d0da9
Matan Rokach <mtn.rkh@gmail.com>
2021-01-14 13:41:00 +0200

    Colors constants

---
+++ src/constants/Colors.js
@@ -0,0 +1,15 @@
+
+
+const Colors = {
+	Blue: '#4d5bf2',
+	Grey: '#aabbcc',
+	White: '#fff',
+};
+
+const AppColors = {
+	Primary: Colors.Blue,
+	Grey: Colors.Grey,
+	White: Colors.White,
+}
+
+export default AppColors;
